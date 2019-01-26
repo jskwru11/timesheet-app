616f08452338182e2690eb70b1b5420bb2144d63
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-26 12:50:58 -0500

    backend update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,53 +1,56 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyCVLD6OlsrBQYAZP6QhN7TrmWsUgyHApHU",
-    authDomain: "employeedata-6ce71.firebaseapp.com",
-    databaseURL: "https://employeedata-6ce71.firebaseio.com",
-    projectId: "employeedata-6ce71",
-    storageBucket: "employeedata-6ce71.appspot.com",
-    messagingSenderId: "737073428957"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
+$(document).ready(function () {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCVLD6OlsrBQYAZP6QhN7TrmWsUgyHApHU",
+        authDomain: "employeedata-6ce71.firebaseapp.com",
+        databaseURL: "https://employeedata-6ce71.firebaseio.com",
+        projectId: "employeedata-6ce71",
+        storageBucket: "employeedata-6ce71.appspot.com",
+        messagingSenderId: "737073428957"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
 
-var employeeName;
-var employeeRole;
-var employeeStartDate;
-var employeeRate;
-
-
-//calculate on fly: employeeMonthsWorked, employeeTotalBilled
+    var employeeName;
+    var employeeRole;
+    var employeeStartDate;
+    var employeeRate;
 
 
-//addEmployee function
-$("#btn-add").on("click", function (event) {
-    console.log("here");
-    employeeName = $("#employee-name").val().trim();
-    employeeRole = $("#employee-role").val().trim();
-    employeeRate = $("#employee-rate").val().trim();
-    employeeStartDate = $("#employee-start-date").val().trim();
-    console.log(employeeName, employeeRole, employeeRate, employeeStartDate);
-    var employeeData = {
-        employeeName: employeeName,
-        employeeRole: employeeRole,
-        employeeRate: employeeRate,
-        employeeStartDate: employeeStartDate
-    }
-    database.ref().push({ employeeData });
+    //calculate on fly: employeeMonthsWorked, employeeTotalBilled
+
+
+    //addEmployee function
+    $("#btn-add").on("click", function (event) {
+        console.log("here");
+        employeeName = $("#employee-name").val().trim();
+        employeeRole = $("#employee-role").val().trim();
+        employeeRate = $("#employee-rate").val().trim();
+        employeeStartDate = $("#employee-start-date").val().trim();
+        console.log(employeeName, employeeRole, employeeRate, employeeStartDate);
+        var employeeData = {
+            employeeName: employeeName,
+            employeeRole: employeeRole,
+            employeeRate: employeeRate,
+            employeeStartDate: employeeStartDate
+        }
+        database.ref().push({ employeeData });
+    });
+
+    //listen for added child function
+    database.ref().on("child_added", function (snapshot) {
+        // snapshot.forEach((child) => {
+        let theEmployeeName = child.employeeName;
+        let theEmployeeRole = child.employeeRole;
+        let theEmployeeStartDate = child.employeeStartDate;
+        let theEmployeeRate = child.employeeRate;
+        // });
+        console.log("Name: " + theEmployeeName + ", Role: " + theEmployeeRole + ", Start Date: " + theEmployeeStartDate + ", Rate: " + theEmployeeRate);
+    }, function (errorObject) {
+        console.log("entries-error: " + errorObject.code);
+    });
+
+
+    //function for moment.js to calculate employeeMonthsWorked
+
 });
-
-//listen for added child function
-database.ref().on("child_added", function (snapshot) {
-    // snapshot.forEach((child) => {
-    let theEmployeeName = child.employeeName;
-    let theEmployeeRole = child.employeeRole;
-    let theEmployeeStartDate = child.employeeStartDate;
-    let theEmployeeRate = child.employeeRate;
-    // });
-    console.log("Name: " + theEmployeeName + ", Role: " + theEmployeeRole + ", Start Date: " + theEmployeeStartDate + ", Rate: " + theEmployeeRate);
-}, function (errorObject) {
-    console.log("entries-error: " + errorObject.code);
-});
-
-
-//function for moment.js to calculate employeeMonthsWorked
